c06e54f1bd7d01f35ed0be5bf403d96bf730afed
Your Name <anneserocki@gmail.comom>
2022-07-20 14:15:18 -0700

    finish6

--- js/06-countdown.js
+++ js/06-countdown.js
@@ -1,6 +1,7 @@
 // count down
 
 let enterNumber = parseInt(prompt('Please enter a number'));
+let again= window.prompt("Repeat entering? ", enterNumber);
 
 for (i = enterNumber; i>=0; i--) {
     document.write(i + `<br>`);   
