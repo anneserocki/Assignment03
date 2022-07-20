b9196b792e8678abcebb744b7e67469a1cc4585c
Your Name <anneserocki@gmail.comom>
2022-07-20 11:50:28 -0700

    finish3

---
+++ js/03-triangle.js
@@ -0,0 +1,10 @@
+// variables
+let i;
+let j = 0;
+
+for (i=1; i<8; i++) {
+for (j=0; j<i; j++) {
+   document.write(` # `);    
+   }; 
+   document.write(`<br>`);      
+}
---
+++ js/04-odd-or-even.js
@@ -0,0 +1,9 @@
+let evenNumber, oddNumber;
+
+for (i=0; i<16; i++){
+    if (i % 2 == 0) {
+        console.log(`${i} is an even`);        
+    } else {
+        console.log(`${i} is an odd`);
+    };
+}
